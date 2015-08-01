a6698566675a03e573d2becb2b29acd050179727
ilario <ilario.sanse@gmail.com>
2015-08-01 19:26:03 +1200

    HangView: fixed display problems. Keyboard: centerd smaller rows.

--- app/src/main/java/com/altervista/lemaialone/hangsans/HangFragment.java
+++ app/src/main/java/com/altervista/lemaialone/hangsans/HangFragment.java
@@ -1,7 +1,7 @@
 package com.altervista.lemaialone.hangsans;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -73,7 +73,7 @@ public class HangFragment extends Fragment implements OnLetterListener, OnClickL
 
 	@Override
 	public void onClick(View view){
-		button.setVisibility(View.GONE);
+		button.setVisibility(View.INVISIBLE);
 		initQuestion();
 	}
 
--- app/src/main/java/com/altervista/lemaialone/hangsans/ui/HangView.java
+++ app/src/main/java/com/altervista/lemaialone/hangsans/ui/HangView.java
@@ -3,16 +3,13 @@ package com.altervista.lemaialone.hangsans.ui;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.widget.ImageView;
-import android.support.annotation.NonNull;
 
 import com.altervista.lemaialone.hangsans.R;
 
@@ -20,10 +17,8 @@ import java.nio.charset.CharacterCodingException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.util.TypedValue.*;
-
-//(12,241) - (272, 286)
-// 286x299 -> 572x598
+import static android.util.TypedValue.COMPLEX_UNIT_DIP;
+import static android.util.TypedValue.applyDimension;
 
 /**
  * Hangman image with word included.
@@ -32,11 +27,10 @@ import static android.util.TypedValue.*;
 public class HangView extends ImageView {
 	private final static String PLACEHOLDER = "_";
 
-	private RectF textRect = new RectF();
-	private RectF originalRect;
+	private final RectF textRect;
 	private String word = "";
 	private String toDraw = "";
-	private Paint paint;
+	private final Paint paint;
 	private float textBottom;
 	private List<Character> guessed = new ArrayList<>();
 	private int imageIndex = 0;
@@ -53,10 +47,8 @@ public class HangView extends ImageView {
 		super(context, attrs, defStyleAttr);
 		paint = new Paint();
 		paint.setTextAlign(Paint.Align.CENTER);
-		paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-		paint.setFakeBoldText(true);
 		DisplayMetrics metrics = getResources().getDisplayMetrics();
-		originalRect = new RectF(
+		textRect = new RectF(
 				applyDimension(COMPLEX_UNIT_DIP, 12, metrics),
 				applyDimension(COMPLEX_UNIT_DIP, 241, metrics),
 				applyDimension(COMPLEX_UNIT_DIP, 272, metrics),
@@ -86,8 +78,12 @@ public class HangView extends ImageView {
 		}
 		finally{
 			computeStringToDraw();
-			invalidate((int)textRect.left, (int)textRect.top,
-					(int)textRect.right, (int)textRect.bottom);
+//			RectF rect = new RectF();
+//			Rect intRect = new Rect();
+//			getImageMatrix().mapRect(textRect, rect);
+//			rect.roundOut(intRect);
+//			invalidate(intRect);
+			invalidate();
 		}
 	}
 
@@ -97,6 +93,18 @@ public class HangView extends ImageView {
 
 	private void computeStringToDraw(){
 		toDraw = word.replaceAll(makeHideRegex(), PLACEHOLDER);
+
+		float textSize = 8f, textHeight;
+		Rect bounds = new Rect();
+		do{
+			paint.setTextSize(++textSize);
+			Paint.FontMetrics metrics = paint.getFontMetrics();
+			paint.getTextBounds(word, 0, word.length(), bounds);
+			textBottom = metrics.bottom;
+			textHeight = metrics.bottom-metrics.top;
+		} while(textRect.height() > textHeight && textRect.width() > bounds.width());
+
+		paint.setTextSize(--textSize);
 	}
 
 	private boolean refreshImage(){
@@ -121,29 +129,9 @@ public class HangView extends ImageView {
 	}
 
 	@Override
-	public void setImageDrawable(Drawable drawable){
-		super.setImageDrawable(drawable);
-
-		Matrix matrix = getImageMatrix();
-		matrix.mapRect(textRect, originalRect);
-
-		float textSize = 8f, textHeight;
-		Rect bounds = new Rect();
-		do{
-			paint.setTextSize(++textSize);
-			Paint.FontMetrics metrics = paint.getFontMetrics();
-			paint.getTextBounds(word, 0, word.length(), bounds);
-			textBottom = metrics.bottom;
-			textHeight = metrics.bottom-metrics.top;
-		} while(textRect.height() > textHeight && textRect.width() > bounds.width());
-
-		paint.setTextSize(--textSize);
-	}
-
-	@Override
 	protected void onDraw(@NonNull Canvas canvas){
 		super.onDraw(canvas);
-		Matrix m = getImageMatrix();
+		canvas.concat(getImageMatrix());
 		canvas.drawText(toDraw, textRect.centerX(), textRect.bottom - textBottom, paint);
 	}
 }
